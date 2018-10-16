a241d51d12215e25498d493105205e8678de4a6c
April Copes <antdevelopment1@gmail.com>
2018-10-16 18:48:52 -0400

    Base outline for Api retrieval

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// function getPuppy() {
+//     fetch('http://my-little-cors-proxy.herokuapp.com/https://dog.ceo/api/breeds/image/random')
+//         .then(response => response.json())
+//         .then(data => data.message)
+//         .then(puppyImage)  
+// }
+
+// function puppyImage(image) {
+//     console.log(image);
+//     let newImage = document.createElement('img');
+//     newImage.setAttribute('src', image)
+//     puppyContainer.append(newImage);
+// }
+
+// function clickButton() {
+//     puppyButton.addEventListener('click', getPuppy)
+// }
+// clickButton();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
